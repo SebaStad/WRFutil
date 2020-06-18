297e27def344aec44f9915a8936d88c86f992a5a
Stadler <sebastian.stadler@ibp.fraunhofer.de>
2020-06-18 13:34:13 +0200

    created ncvar_subs, rewrote some funcs

--- R/get_pot_temp.R
+++ R/get_pot_temp.R
@@ -8,39 +8,32 @@
 #' @export
 #'
 #' @examples
-get_pot_temp_2D <- function(ncfile, tstep, zlayer) {
-  nc_file <- ncdf4::nc_open(ncfile)
+get_pot_temp_2D <- function(ncfile, x_start = 1, y_start= 1, z_start= 1, t_start= 1,
+                            x_len, y_len, z_len= 1, t_len= 1) {
+  nc <- ncdf4::nc_open(ncfile)
+
+  sdf <- data.frame(x=x_start,
+                    y=y_start,
+                    z=z_start,
+                    t=t_start,
+                    stringsAsFactors = F)
+
+  ldf <- data.frame(x=x_len,
+                    y=y_len,
+                    z=z_len,
+                    t=t_len,
+                    stringsAsFactors = F)
 
   Pa_to_hPA <- 100
 
-  start_time <- tstep[1]
-  duration <- length(tstep)
-
-  start_layer <- zlayer
-  heights <- length(zlayer)
-
-  x_len <- nc_file$dim$west_east$len
-  y_len <- nc_file$dim$south_north$len
-
-  t_data <- ncdf4::ncvar_get(nc_file, "T",
-    start = c(1, 1, start_layer, start_time),
-    count = c(x_len, y_len, heights, duration)
-  )
-
-  p_data <- ncdf4::ncvar_get(nc_file, "P",
-    start = c(1, 1, start_layer, start_time),
-    count = c(x_len, y_len, heights, duration)
-  ) / Pa_to_hPA
-
-  pp_data <- ncdf4::ncvar_get(nc_file, "PB",
-    start = c(1, 1, start_layer, start_time),
-    count = c(x_len, y_len, heights, duration)
-  ) / Pa_to_hPA
+  t_data  <- ncvar_subs("T", start_df = sdf, len_df = ldf, nc = nc )
+  p_data  <- ncvar_subs("P", start_df = sdf, len_df = ldf, nc = nc)/ Pa_to_hPA
+  pp_data <- ncvar_subs("PB", start_df = sdf, len_df = ldf, nc = nc)/Pa_to_hPA
 
   t_d300 <- t_data + 300
-  ptot <- p_data + pp_data
+  ptot   <- p_data + pp_data
 
-  temp <- t_d300 * (ptot / 1000)^(2 / 7)
+  temp   <- t_d300 * (ptot / 1000)^(2 / 7)
 
   nc_close(nc_file)
 
